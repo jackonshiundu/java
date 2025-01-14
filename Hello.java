86817cc03f4b285810de11f724d7a46f159970a6
jackonshiundu <jackonshiundu2019@gmail.com>
2025-01-14 22:02:27 +0300

    data-types-in-java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello {
+    public static void main(String a[]){
+        int num1 = 10;
+        int num2 = 3;
+        int results =num1 + num2;
+        System.out.print(results);
+    }
+}
