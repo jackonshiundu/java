eb52675e59547f62c5dc4dd0aba224d59e367716
jackonshiundu <jackonshiundu2019@gmail.com>
2025-01-17 19:06:13 +0300

    multi-dimention-arrays

--- Classes.java
+++ Classes.java
@@ -19,5 +19,46 @@ public class Classes{
         Calculator calc=new Calculator();
         int results=calc.add(4,5,5);
         System.out.println(results);
+
+        //arrays
+        //Creatign an array syntax 1
+        int nums[]={3,5,8,9};
+        //re assigning
+        nums[1]=10;
+        //creatign An Array syntax 2
+        int nums2[]=new int[5];
+        //Before reassigning the values are 0
+        nums2[0]=89;
+        nums2[1]=2;
+        nums2[2]=3;
+        nums2[3]=9;
+        nums2[4]=39;
+        for(int i=0;i<=4;i++){
+
+            System.out.println(nums2[i]);
+        }
+        //Multi Dimension Arrays
+        int multiDimenArray [][]=new int [3][4];
+        //Initializing the values dynamically
+        for(int  i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                multiDimenArray[i][j]=(int)(Math.random()*100);
+
+            }
+        }
+        //Printing the values
+        for(int  i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(multiDimenArray[i][j]+" ");
+            }
+            System.out.println();
+        }
+        //Enhanced Loop
+        for(int n[]:multiDimenArray){
+            for(int m:n){
+                System.out.print(m + " ");
+            }
+            System.err.println();
+        }
     }
 }
